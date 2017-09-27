2f080ba570cf45c69fe97a90ef143f3c1f63f9b1
Michael Lipscombe <mlipscombe@outlook.com>
2017-09-27 11:33:13 -0600

    Added manifest for PCF
    Fixed package issues with maven

--- src/main/java/com/mike/Service/StudentService.java
+++ src/main/java/com/mike/Service/StudentService.java
@@ -12,7 +12,7 @@ import java.util.Collection;
 public class StudentService {
 
     @Autowired
-    @Qualifier("mongoData")
+    @Qualifier("fakeData")
     private StudentDao studentDao;
 
     public Collection<Student> getAllStudents(){
